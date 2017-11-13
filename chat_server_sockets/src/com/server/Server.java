a116587a1680861daf00079ae7531850547cb572
ShrubinS <shrubin93.skn@gmail.com>
2017-11-13 20:03:37 +0000

    added code

---
+++ chat_server_sockets/src/com/server/Server.java
@@ -0,0 +1,30 @@
+package com.server;
+
+import com.server.threads.*;
+
+import java.io.*;
+import java.net.*;
+
+
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 1) {
+            System.err.println("Usage: java MultiServer <port number>");
+            System.exit(1);
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+        boolean listening = true;
+
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            while (listening) {
+                new MultiServerThread(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Could not listen on port " + portNumber);
+            System.exit(-1);
+        }
+    }
+}
